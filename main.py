a4115e20cfb62aea976eb446f84624b3f71a72b1
jensdanb <56897399+jensdanb@users.noreply.github.com>
2025-01-27 00:05:41 +0100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,36 @@
+from pypdf import PdfReader, PdfWriter
+from glob import glob
+from time import sleep
+
+
+input_file_names = glob('*input/*.pdf', recursive=True)
+input_pdfs = [PdfReader(name) for name in input_file_names]
+
+if len(input_pdfs) == 0:
+    print("No pdf files found in input folder. Exiting in 3 seconds")
+    sleep(3)
+    exit()
+
+
+pages_per_pdf = [len(pdf.pages) for pdf in input_pdfs]
+
+if not len(set(pages_per_pdf)) == 1:
+    print("Not all files have the same number of pages. Exiting in 3 seconds")
+    print("In order, number of pages are: " + repr(pages_per_pdf))
+    sleep(3)
+    exit()
+
+number_of_pages = pages_per_pdf[0]
+
+
+for page_nr in range(number_of_pages):
+    writer = PdfWriter()
+    
+    for input_pdf in input_pdfs:
+        writer.append(input_pdf, [page_nr])
+        print(len(writer.pages))
+    
+    output_file = open("output/doc_" + str(page_nr+1) + ".pdf", "wb")
+    writer.write(output_file)
+    writer.close()
+    output_file.close()
